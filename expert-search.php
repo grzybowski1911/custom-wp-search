e10ca4ec3b96b28a96f5bc0513a113e4df181d20
grzybowski1911 <grzybowski1911@gmail.com>
2022-04-08 11:03:27 -0700

    update placeholder text and aria labels

--- expert-search.php
+++ expert-search.php
@@ -16,19 +16,19 @@ if(!empty($industries )) {
 
 <form method="get" action="<?php echo get_post_type_archive_link('experts'); ?>" role="search">
   <div class="form-group">
-    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Name or Keyword">
+    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Search by name or keyword" aria-label="Search by name or keyword">
     <label for="keyword">Search By Keyword or Name</label>
   </div>
   <div class="form-group">
-    <input type="text" class="form-control" id="speciality" name="speciality" placeholder="Name or Keyword" aria-label="Select Specialty">
+    <input type="text" class="form-control" id="speciality" name="speciality" placeholder="Search by speciality" aria-label="Search by speciality">
     <label for="speciality">Search By Speciality</label>
   </div>
   <div class="form-group">
-    <input type="text" class="form-control" id="industry_select" name="industry_select" aria-label="Select Industry">
+    <input type="text" class="form-control" id="industry_select" name="industry_select" placeholder="Search by industry" aria-label="Search by industry">
     <label for="industry_select">Select Industry</label>
   </div>
   <div class="form-group">
-    <input type="text" class="form-control" id="location_select" name="location_select" aria-label="Select Location">
+    <input type="text" class="form-control" id="location_select" name="location_select" placeholder="Search by location" aria-label="Search by location (state)">
     <label for="location_select">Select Location</label>
   </div>
   <!--<div class="form-group">
